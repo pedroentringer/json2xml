b985eb7c1d9415c958cfa2799a8a074f7793dba8
Pedro Entringer <pedroentringer@MacBook-Air-de-Pedro.local>
2020-08-13 22:51:47 -0300

    support undefined type

--- src/utils/typeNode.ts
+++ src/utils/typeNode.ts
@@ -5,15 +5,16 @@ export const type = TYPE
 export const getTypeNode = (node:any) => {
   const type = typeof node
   
+  if(type === 'undefined') return TYPE.UNDEFINED
   if(type === 'boolean') return TYPE.SINGLE
   if(type === 'number') return TYPE.SINGLE
+  if(type === 'string') return TYPE.SINGLE
   if(type === 'object') {
 
     if(Array.isArray(node)) return TYPE.ARRAY
 
     return TYPE.OBJECT
   }
-  if(type === 'string') return TYPE.SINGLE
   
   throw new Error(`Type ${type} not supported`)
 }
